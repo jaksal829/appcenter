8008f5259fc0f3df81222df7a7ba31fe5ea3a8f9
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2019-12-09 00:24:03 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
   </script>
 </body>
 </html>
-<?php
-        echo (" 초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0']);
- 
-?>
+<?php //while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo (" 초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
+  //  } 
+  ?>
