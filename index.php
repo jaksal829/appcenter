ede66c9328078383ab4d3d7646481cc9bb7e387d
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2019-12-09 00:07:51 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -9,22 +9,16 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-?>
-
-
-<?php
    $tsql= "SELECT t_pm1_0, t_pm10, t_pm2_5 FROM dust WHERE EventProcessedUtcTime = (SELECT MAX(EventProcessedUtcTime) FROM dust)";
    $getResults= sqlsrv_query($conn, $tsql);
    // echo ("Reading data from table".PHP_EOL);
    if ($getResults == FALSE){
        echo (sqlsrv_errors());
    }
-   while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-        echo ("초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
-    }
-    sqlsrv_free_stmt($getResults);
+   
    
 ?>
+
 
     <!DOCTYPE html>
 <html>
@@ -143,4 +137,7 @@
   </script>
 </body>
 </html>
-
+<?php while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo ("초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults); ?>
