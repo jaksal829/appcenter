f51a38b9592de9e6742e45f3efe2f9c2cda898d6
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2019-12-09 01:00:57 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -126,15 +126,7 @@
                 });
                 kakao.maps.event.addListener(marker, 'click', makeClick(map,marker,infowindow));
             }
-            <?php 
-            while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-                echo (" 초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
-                $x = $row['t_pm2_5'];
-                $y = $row['t_pm10'];
-                $z = $row['t_pm1_0'];
-            } 
-           
-            ?>
+          
             <p> 안라미ㅜㅏㅓㅎ마ㅓㄴㅇ후ㅏㅓㄴㅇ후ㅏㄴ</p>
             // 마커가 지도 위에 표시되도록 설정합니다
     
@@ -148,4 +140,9 @@
   </script>
 </body>
 </html>
-
+<?php 
+            while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+                echo (" 초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
+            } 
+           
+            ?>
