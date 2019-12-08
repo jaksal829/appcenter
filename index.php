411878503d8b3d9da94b57c2369a6e81113bd6c1
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2019-12-08 23:58:16 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
 ?>
 
 
+<?php
+    $tsql= "SELECT t_pm1_0, t_pm10, t_pm2_5 FROM dust WHERE EventProcessedUtcTime = (SELECT MAX(EventProcessedUtcTime) FROM dust)";
+    $getResults= sqlsrv_query($conn, $tsql);
+    // echo ("Reading data from table".PHP_EOL);
+    if ($getResults == FALSE){
+        echo (sqlsrv_errors());
+    }
+        
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo ("초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
+
     <!DOCTYPE html>
 <html>
 <head>
@@ -36,19 +50,7 @@
             var map = new kakao.maps.Map(mapContainer, mapOption);
             
 
-            <?php
-                $tsql= "SELECT t_pm1_0, t_pm10, t_pm2_5 FROM dust WHERE EventProcessedUtcTime = (SELECT MAX(EventProcessedUtcTime) FROM dust)";
-                $getResults= sqlsrv_query($conn, $tsql);
-               // echo ("Reading data from table".PHP_EOL);
-                if ($getResults == FALSE){
-                    echo (sqlsrv_errors());
-                }
-                    
-                while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-                    echo ("초미세먼지 : ".$row['t_pm2_5']." / 미세먼지 : ".$row['t_pm10']." / 극초미세먼지 : ".$row['t_pm1_0'].PHP_EOL);
-                }
-                sqlsrv_free_stmt($getResults);
-            ?>
+            
 
             // 마커가 표시될 위치입니다 
             var markerPosition  = [ 
